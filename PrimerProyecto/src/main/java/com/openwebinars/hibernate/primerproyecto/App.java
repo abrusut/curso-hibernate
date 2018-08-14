c94de5110e4a266ef46fc2cd0349cb6742f80e4c
Andres <andresbrusutti@gmail.com>
2018-08-14 16:55:03 -0300

    init commit

---
+++ PrimerProyecto/src/main/java/com/openwebinars/hibernate/primerproyecto/App.java
@@ -0,0 +1,40 @@
+package com.openwebinars.hibernate.primerproyecto;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	// Forma vieja (levanta el hibernate.cfg.xml del src/main/resources por defecto
+        // SessionFactory sf = new Configuration().configure().buildSessionFactory();
+    	
+    	StandardServiceRegistry sr = new StandardServiceRegistryBuilder()
+    			.configure().build();
+    	
+    	SessionFactory sf = new MetadataSources(sr).buildMetadata().buildSessionFactory();
+    	
+    	Session session = sf.openSession();
+    	
+    	User user = new User();
+    	user.setId(1);
+    	user.setUsername("Andres");
+    	user.setUserMensagge("Mensaje hello word");
+    	
+    	User user2 = new User();
+    	user2.setId(1);
+    	user2.setUsername("Juan");
+    	user2.setUserMensagge("Otro hello word");
+    	
+    	session.close();
+    	sf.close();
+    }
+}
---
+++ PrimerProyecto/src/main/java/com/openwebinars/hibernate/primerproyecto/User.java
@@ -0,0 +1,50 @@
+package com.openwebinars.hibernate.primerproyecto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+	public String getUserMensagge() {
+		return userMensagge;
+	}
+
+
+	public void setUserMensagge(String userMensagge) {
+		this.userMensagge = userMensagge;
+	}
+
+
+	private String username;
+	
+	private String userMensagge;
+	
+	
+	public User() {
+		
+	}
+}
