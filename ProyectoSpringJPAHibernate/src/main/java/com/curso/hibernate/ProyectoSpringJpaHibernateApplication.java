eb1ff30a4e6f037d8846fce90658f0924d435eb8
Andres <andresbrusutti@gmail.com>
2018-08-22 16:39:31 -0300

    Primer proyecto SpringBoot JPA Hibernate

---
+++ ProyectoSpringJPAHibernate/src/main/java/com/curso/hibernate/ProyectoSpringJpaHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.curso.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSpringJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSpringJpaHibernateApplication.class, args);
+	}
+}
